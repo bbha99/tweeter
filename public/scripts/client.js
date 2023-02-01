ae405a0b8386568d87f163e27a7cd86e2796de1b
bbha99 <branha99@gmail.com>
2023-02-01 15:45:22 -0800

    fetch dynamic tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Creates the tweet post object
+const createTweetElement = function(tweetObj) {
+  const $tweet = $(`
+  <article class="tweet">
+  <header>
+    <div class="tweet-header">
+      <img src="${tweetObj.user.avatars}" alt="face sketch">
+      <h3>${tweetObj.user.name}</h3>
+    </div>
+    <h3>${tweetObj.user.handle}</h3>
+  </header>
+  <p>${tweetObj.content.text}</p>
+  <footer class="tweet-footer">
+    <p>${timeago.format(tweetObj.created_at)}</p>
+    <div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-repeat"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+</article>`);
+  return $tweet;
+}
+
+// Displays all the tweet post
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  tweets.forEach(tweetData => {
+    const $tweet = createTweetElement(tweetData);
+    $('#tweets-container').append($tweet);
+  });
+}
+
+$(document).ready(function() {
+
+  $newTweet = $('form').submit(function (event) {
+    event.preventDefault();
+    const queryString = $(this).serialize();
+    $.ajax('/tweets/', { method: 'POST' , data:queryString})
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets/', { method: 'GET' })
+    .then(function (res) {
+      renderTweets(res);
+    });
+  }
+
+  loadTweets();
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
   const textarea = document.getElementById("tweet-text");
   const charLength = 140;
 
+  // Updates the character counter for the tweet form
   textarea.addEventListener("input", function() {
     let textLength = $(this).val().length;
     let counterNode = $(this).parent().children(".tweet-info").children(".counter");
     if (textLength > charLength) {
-      counterNode.css({'color': 'red'});
+      counterNode.addClass("negative-counter");
     } else {
-      counterNode.css({'color': 'black'});
+      counterNode.removeClass("negative-counter");
     }
 
     counterNode.text(charLength - textLength);
